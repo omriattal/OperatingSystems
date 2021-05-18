cfaa2bbf41456114cfd124c4bfded5437dc39ce5
Roee Weiss Lipshitz <roeeweisslip@gmail.com>
2021-05-18 16:35:31 +0300

    Add todo to param.h

--- Assignment3/os212-assignment3/kernel/param.h
+++ Assignment3/os212-assignment3/kernel/param.h
@@ -1,21 +1,21 @@
-#define NPROC        64  // maximum number of processes
-#define NCPU          8  // maximum number of CPUs
-#define NOFILE       16  // open files per process
-#define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
-#define NDEV         10  // maximum major device number
-#define ROOTDEV       1  // device number of file system root disk
-#define MAXARG       32  // max exec arguments
-#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
-#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
-#define MAXPATH      128   // maximum file path name
+#define NPROC 64                  // maximum number of processes
+#define NCPU 8                    // maximum number of CPUs
+#define NOFILE 16                 // open files per process
+#define NFILE 100                 // open files per system
+#define NINODE 50                 // maximum number of active i-nodes
+#define NDEV 10                   // maximum major device number
+#define ROOTDEV 1                 // device number of file system root disk
+#define MAXARG 32                 // max exec arguments
+#define MAXOPBLOCKS 10            // max # of blocks any FS op writes
+#define LOGSIZE (MAXOPBLOCKS * 3) // max data blocks in on-disk log
+#define NBUF (MAXOPBLOCKS * 3)    // size of disk block cache
+#define FSSIZE 1000               // size of file system in blocks
+#define MAXPATH 128               // maximum file path name
 
 // ADDED: page constants
 #define MAX_PSYC_PAGES 16
 #define MAX_TOTAL_PAGES 32
-#define SHELL_PID 2
+#define SHELL_PID 2 // TODO: consider deleting this define
 #define NO_FILE 0
 #define LOADFAULT 13
 #define STOREFAULT 15
