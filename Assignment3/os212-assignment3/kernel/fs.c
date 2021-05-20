0ed4333fded4a81b7a6597b7dc1e5b75a8472a1c
Roee Weiss Lipshitz <roeeweisslip@gmail.com>
2021-05-20 12:08:32 +0300

    fix bug in copySwapFile

--- Assignment3/os212-assignment3/kernel/fs.c
+++ Assignment3/os212-assignment3/kernel/fs.c
@@ -830,15 +830,14 @@ int copySwapFile(struct proc *dst, struct proc *src)
 {
     if (dst == 0 || src == 0 || dst->swapFile == 0 || src->swapFile == 0)
         return -1;
-    int max_file_size = PGSIZE * MAX_SWAP_PAGES;
     char *buffer = (char *)kalloc();
     for (struct swap_page *swpg = src->swap_pages; swpg < &src->swap_pages[MAX_SWAP_PAGES]; swpg++)
     {
         if(swpg->state == PG_FREE)
             continue;
-        if (readFromSwapFile(src, buffer, swpg->swap_location, max_file_size) < 0)
+        if (readFromSwapFile(src, buffer, swpg->swap_location, PGSIZE) < 0)
             return -1;
-        if (writeToSwapFile(dst, buffer, swpg->swap_location, max_file_size) < 0)
+        if (writeToSwapFile(dst, buffer, swpg->swap_location, PGSIZE) < 0)
             return -1;
     }
     kfree(buffer);
